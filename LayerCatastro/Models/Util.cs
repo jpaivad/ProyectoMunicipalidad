dbc4b156a4fb434d4f9c3ec1f838f15fbe328320
ericsifuentes <ericgsc18@gmail.com>
2015-08-31 01:42:49 -0500

    Inicio Gestión Municipal
    Se agrego la pagina maestra para utilizar con el motor razor y se
    configuro para que funcione el modulo de gestion municipal.

---
+++ LayerCatastro/Models/Util.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Models
+{
+    public class Util
+    {
+        //Obtiene Titulo de la Pagina
+        public string getTitle()
+        {
+            return "Municipalidad San Isidro";
+        }
+
+        //Obtiene Ruta Pagina
+        public string getPath()
+        {
+            return HttpContext.Current.Request.Url.Scheme + System.Uri.SchemeDelimiter + HttpContext.Current.Request.Url.Authority + HttpContext.Current.Request.ApplicationPath + ((HttpContext.Current.Request.ApplicationPath.EndsWith("/")) ? "" : "/");
+        }
+
+        //Obtenemos la ruta fisica
+        public string getPhysicalPath()
+        {
+            return HttpContext.Current.Request.PhysicalApplicationPath;
+        }
+
+        //Obtenemos la ruta de la imagen
+        public string getImage(string image)
+        {
+            return this.getPath() + "Content/img/" + image;
+        }
+    }
+}
